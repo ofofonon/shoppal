eb902a7142f4bf0cf90e9df2a1ddad41eee7f391
ofofonon <ofinaboy1@gmail.com>
2025-04-28 12:58:16 -0700

    fade

--- hto.js
+++ hto.js
@@ -1,4 +1,5 @@
 const placeholder = document.getElementById('video-placeholder');
+const placeholder2 = document.getElementById('video-placeholder2');
 
   placeholder.addEventListener('click', () => {
     // 1. Create the <video> element
@@ -13,3 +14,17 @@ const placeholder = document.getElementById('video-placeholder');
     placeholder.innerHTML = '';
     placeholder.appendChild(vid);
   });
+
+  placeholder2.addEventListener('click', () => {
+    // 1. Create the <video> element
+    const vid = document.createElement('video');
+    vid.src = 'hto/VID-20250428-WA0020.mp4';
+    vid.controls = true;
+    vid.autoplay = true;
+    vid.width = placeholder2.clientWidth;
+    vid.height = placeholder2.clientHeight;
+
+    // 2. Swap out the placeholder
+    placeholder2.innerHTML = '';
+    placeholder2.appendChild(vid);
+  })
